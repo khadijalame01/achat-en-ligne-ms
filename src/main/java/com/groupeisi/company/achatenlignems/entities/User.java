e108d5ede13c75f4f8094ee4e764615de8acfeef
macbook air <macbookair@macbooks-air.home>
2026-03-18 03:57:23 +0000

    Commit avec les images

--- src/main/java/com/groupeisi/company/achatenlignems/entities/User.java
+++ src/main/java/com/groupeisi/company/achatenlignems/entities/User.java
@@ -1,12 +1,22 @@
 package com.groupeisi.company.achatenlignems.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
-public class User extends BaseEntity{
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email"),
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "phone")
+})
+public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,11 +32,12 @@ public class User extends BaseEntity{
     @Column(name = "is_enabled", columnDefinition = "bit not null default 0")
     private boolean isEnabled = true;
     @Column(name = "account_is_enabled", columnDefinition = "bit not null default 1")
-    private boolean accountIsEnabled = true ;
+    private boolean accountIsEnabled = true;
     @Column(name = "account_is_not_expired", columnDefinition = "bit not null default 1")
     private boolean accountIsNotExpired = true;
     @Column(name = "account_is_not_locked", columnDefinition = "bit not null default 1")
     private boolean accountIsNotLocked = true;
     @Column(name = "credential_not_expired", columnDefinition = "bit not null default 1")
     private boolean credentialNotExpired = true;
+
 }
